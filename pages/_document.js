350483fb22d88cf52b79eb0f0c5d9c79e05cdac8
Ethan Liang <e3lcool@gmail.com>
2024-12-30 22:50:45 -0800

    fix crossOrigin

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
+                    <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
 
                     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@700&family=Open+Sans&display=swap" rel="preload" as="style" />
                     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Montserrat:wght@700&family=Open+Sans&display=swap" rel="stylesheet" />
